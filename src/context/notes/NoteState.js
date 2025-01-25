e024695e683e57a1ae4c6c518fedeb50416d43a9
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2025-01-25 15:56:43 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://code-note-backend.onrender.com";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
   // Get all notes..
