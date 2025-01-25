d5b496906b6d9193f70be8988b05316e8ecbbc0e
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2025-01-25 16:27:02 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://code-note-backend.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
